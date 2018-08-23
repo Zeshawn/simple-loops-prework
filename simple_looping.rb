d345dcbc005bc98b55f50e430c42466736b0b65b
Zeshawn Khan <zeshawnk@gmail.com>
2018-08-23 04:41:30 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -49,10 +49,11 @@ def until_iterator(number_of_times)
   
   counter = 0
   
-  until counter == number_of_times do
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  puts phrase
-  counter =+ 1
+  count = 0
+  until count == number_of_times do
+    puts phrase
+    count += 1
   end
 
 end
